74f8a46f9a4a5116ed1aabee24ebb97e29be44f8
behuFTP <benjamin.dals.hughes@gmail.com>
2019-07-10 15:27:27 +0200

    cleanup

--- src/server/index.js
+++ src/server/index.js
@@ -21,4 +21,12 @@ router.use("/users", userRouter);
 
 app.use("/api", router);
 
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./../../dist/index.html"), function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 app.listen(process.env.PORT || 5000);
